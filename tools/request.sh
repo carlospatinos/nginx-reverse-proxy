b43b64db1d9248a66bc134198e521a1e929f2af7
Carlos Patino <carlospatinos@gmail.com>
2022-01-19 09:53:05 +0000

    openshift testing starting

--- tools/request.sh
+++ tools/request.sh
@@ -18,5 +18,15 @@ curl -s -H "Host: service_b.example.com" http://localhost:8080/
 # curl -k -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" https://localhost:444/get_gateway
 # curl -v -k -X POST -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" -d '{"nombre": "ivan", "trabajo": "pilloni" }' https://localhost:444/ademo
 
+# Openshift http port
+URL='http://reversenginx-root-reverse-proxy.mycluster-mex01-b3c-4x16-18f5cff1396700a3eb11f3582dee8624-0000.mex01.containers.appdomain.cloud'
 
-curl -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" http://reverse-route-http-demo3-reverse-proxy.mycluster-mex01-b3c-4x16-18f5cff1396700a3eb11f3582dee8624-0000.mex01.containers.appdomain.cloud/demo3
+
+# CURLs
+curl -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" $URL/get_http_https
+curl -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" $URL/get_http_http
+curl -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" $URL/get_gateway
+curl -X POST -s -H "X-IBM-Client-Id: cf48ee8ca22bda0d29aff0bef8eb5e2e" -d '{"nombre": "ivan", "trabajo": "pilloni" }' $URL/ademo
+
+# Openshift http port
+URL='https://reversenginx-https-root-reverse-proxy.mycluster-mex01-b3c-4x16-18f5cff1396700a3eb11f3582dee8624-0000.mex01.containers.appdomain.cloud'
